9f223aca13e97bc9e89f99b2432970f8641222bc
indunil jay <indunil.jayz@gmail.com>
2025-10-26 21:24:37 +0530

    feat(todos) : add domain entity

---
+++ Skolar.Domain/Todo.cs
@@ -0,0 +1,53 @@
+﻿namespace Skolar.Domain;
+
+public sealed class Todo
+{
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public string? Description { get; private set; }
+    public string Priority { get; private set; } 
+    public bool IsCompleted { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
+    public DateTime? DueDate { get; private set; }
+    public DateTime? CompletedAt { get; private set; }
+
+    private Todo(
+        Guid id,
+        string title,
+        string? description,
+        string priority,
+        bool isCompleted,
+        DateTime createdAt,
+        DateTime? dueDate
+    )
+    {
+
+        Id = id;
+        Title = title;
+        Description = description;
+        Priority = priority;
+        IsCompleted = isCompleted;
+        CreatedAt = createdAt;
+        DueDate = dueDate;
+    }
+
+    public static Todo Create(
+        string title,
+        string? description = null,
+        string priority = "Normal",
+        DateTime? dueDate = null)
+    {
+        return new Todo(
+            Guid.NewGuid(),
+            title,
+            description,
+            priority,
+            false, 
+            DateTime.UtcNow,
+            dueDate
+        );
+    }
+
+  
+}
