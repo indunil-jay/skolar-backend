48a5a4016f9f9353fcf13b45bdacbfe8ab697f43
indunil jay <indunil.jayz@gmail.com>
2025-10-28 22:20:58 +0530

    feat(todos): add repoistories

---
+++ Skolar.Domain/Todos/ITodoRepository.cs
@@ -0,0 +1,8 @@
+﻿using Skolar.Domain.Primitives;
+
+namespace Skolar.Domain.Todos;
+
+public interface ITodoRepository : IBaseRepository<Todo>
+{
+    Task<IReadOnlyList<Todo>> GetAllAsync(CancellationToken cancellationToken = default);
+}
---
+++ Skolar.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skolar.Domain.Primitives;
+
+namespace Skolar.Infrastructure.Repositories;
+
+internal abstract class BaseRepository<T> : IBaseRepository<T> where T : Entity
+{
+    protected readonly ApplicationDbContext _dbContext;
+
+    protected BaseRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public virtual void Add(T entity)
+    {
+        _dbContext.Add(entity);
+    }
+
+    public virtual void Delete(T entity)
+    {
+        _dbContext.Remove(entity);
+    }
+
+    public virtual async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.Set<T>().FirstOrDefaultAsync(entry=> entry.Id==id, cancellationToken); 
+    }
+}
