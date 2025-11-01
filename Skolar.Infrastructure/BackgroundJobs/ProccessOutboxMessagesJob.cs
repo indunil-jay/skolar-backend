5fd62d25bfb8c3dfe8b445e6a37d0f415d8fca62
indunil jay <indunil.jayz@gmail.com>
2025-11-01 14:20:42 +0530

    feat : add outbox pattern and add background jobs for external event triggering

---
+++ Skolar.Infrastructure/BackgroundJobs/ProccessOutboxMessagesJob.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Quartz;
+using Skolar.Domain.Primitives;
+using Skolar.Infrastructure.Outbox;
+
+namespace Skolar.Infrastructure.BackgroundJobs;
+
+[DisallowConcurrentExecution]
+public class ProccessOutboxMessagesJob : IJob
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly IPublisher _publisher;
+
+    public ProccessOutboxMessagesJob(ApplicationDbContext dbContext, IPublisher publisher)
+    {
+        _dbContext = dbContext;
+        _publisher = publisher;
+    }
+    public async Task Execute(IJobExecutionContext context)
+    {
+        List<OutboxMessage> messages = await _dbContext
+                               .Set<OutboxMessage>()
+                               .Where(m => m.ProcessedOnUtc == null)
+                               .Take(20)
+                               .ToListAsync(context.CancellationToken);
+
+        foreach (OutboxMessage message in messages)
+        {
+            IDomainEvent? domainEvent = JsonConvert.DeserializeObject<IDomainEvent>(message.Content, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All
+            });
+
+            if(domainEvent is null)
+            {
+                //may be all logs
+                continue;
+            }
+
+            await _publisher.Publish(domainEvent, context.CancellationToken);
+            message.ProcessedOnUtc = DateTime.UtcNow;   
+        }
+
+        await _dbContext.SaveChangesAsync();
+
+    }
+}
--- Skolar.Infrastructure/DependencyInjection.cs
+++ Skolar.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Quartz;
 using Skolar.Domain.Primitives;
 using Skolar.Domain.Todos;
+using Skolar.Infrastructure.BackgroundJobs;
 using Skolar.Infrastructure.Interceptors;
 using Skolar.Infrastructure.Repositories;
 
@@ -10,7 +12,7 @@ namespace Skolar.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
 
         services.AddSingleton<ConvertDomainEventsToOutboxMessagesInterceptor>();
@@ -19,7 +21,7 @@ public static class DependencyInjection
         // -------------------------
         var connectionString = configuration.GetConnectionString("DefaultConnection")
                                ?? throw new ArgumentNullException(nameof(configuration), "Connection string 'DefaultConnection' not found.");
-        services.AddDbContext<ApplicationDbContext>((serviceProvider,optionsBuilder) =>
+        services.AddDbContext<ApplicationDbContext>((serviceProvider, optionsBuilder) =>
         {
             var interceptor = serviceProvider.GetService<ConvertDomainEventsToOutboxMessagesInterceptor>();
             optionsBuilder.UseSqlServer(connectionString)
@@ -29,6 +31,24 @@ public static class DependencyInjection
         //Unit Of Work
         services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
 
+        // -------------------------
+        // Background Jobs
+        // -------------------------
+        services.AddQuartz(configure =>
+        {
+            var jobKey = new JobKey(nameof(ProccessOutboxMessagesJob));
+
+            configure.AddJob<ProccessOutboxMessagesJob>(
+                jobKey,
+                (Action<IJobConfigurator>)(opts => opts.WithIdentity(jobKey.Name))
+            )
+            .AddTrigger(trigger => trigger
+                .ForJob(jobKey)
+                .WithIdentity("OutboxJob-trigger")
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(10).RepeatForever()));
+        });
+
+        services.AddQuartzHostedService();
 
         // -------------------------
         // Repositories
