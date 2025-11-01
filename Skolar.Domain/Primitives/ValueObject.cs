2187b8d772a65a0431f0ec2d3de806e42f28375c
indunil jay <indunil.jayz@gmail.com>
2025-11-01 11:12:12 +0530

    feat : add base value object  class

---
+++ Skolar.Domain/Primitives/ValueObject.cs
@@ -0,0 +1,30 @@
+﻿namespace Skolar.Domain.Primitives;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetAtomicValues();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+            return false;
+        var other = (ValueObject)obj;
+        return ValuesAreEqual(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return GetAtomicValues()
+            .Aggregate(default(int), HashCode.Combine);
+    }
+
+    private bool ValuesAreEqual(ValueObject other)
+    {
+        return GetAtomicValues().SequenceEqual(other.GetAtomicValues());
+    }
+
+    public bool Equals(ValueObject? other)
+    {
+        return other is not null && ValuesAreEqual(other);
+    }
+}
